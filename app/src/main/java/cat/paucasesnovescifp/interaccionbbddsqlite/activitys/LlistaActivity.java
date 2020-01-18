663eab7461fd092593b9a5cb6c36befdee3c5011
Miguel Rubio <miguelrubio@paucasesnovescifp.cat>
2020-01-18 12:23:38 +0100

    funcionalidad de la lista pero sin setListAdapter

--- app/src/main/java/cat/paucasesnovescifp/interaccionbbddsqlite/activitys/LlistaActivity.java
+++ app/src/main/java/cat/paucasesnovescifp/interaccionbbddsqlite/activitys/LlistaActivity.java
@@ -3,6 +3,7 @@ package cat.paucasesnovescifp.interaccionbbddsqlite.activitys;
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,20 +14,24 @@ import java.util.HashMap;
 import cat.paucasesnovescifp.interaccionbbddsqlite.DBInterface;
 import cat.paucasesnovescifp.interaccionbbddsqlite.R;
 
-public class LlistaActivity extends ListActivity {
+public class LlistaActivity extends AppCompatActivity {
 
     SimpleAdapter adapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_llista);
 
+        listView = findViewById(R.id.lista_activity);
+
         llistaContactes();
 
     }
 
-    public void llistaContactes(){
+    //No funciona poniendo esta clase con ListActivity
+    /*public void llistaContactes(){
 
         DBInterface bd;
         bd = new DBInterface(getApplicationContext());
@@ -44,9 +49,30 @@ public class LlistaActivity extends ListActivity {
         }
 
         bd.tanca();
-        adapter = new SimpleAdapter(getApplicationContext(), llista, R.layout.activity_llista, new String[]{"id","nom","email"},new int[]{R.id.textViewIdLlista,R.id.textViewNomLlista,R.id.textViewEmailLlista});
+        adapter = new SimpleAdapter(getApplicationContext(), llista, R.layout.activity_item_lista, new String[]{"id","nom","email"},new int[]{R.id.id_item_lista,R.id.nombre_item_lista,R.id.email_item_lista});
         setListAdapter(adapter);
 
+    }*/
+
+    public void llistaContactes(){
+        DBInterface bd;
+        bd = new DBInterface(getApplicationContext());
+        bd.obre();
+        Cursor c = bd.obtenirTotsElsContactes();
+        c.moveToFirst();
+        ArrayList<HashMap<String,String>> llista = new ArrayList<HashMap<String, String>>();
+        while(!c.isAfterLast()){
+            HashMap<String,String> map = new HashMap<String, String>();
+            map.put("id",c.getString(0));
+            map.put("nom",c.getString(1));
+            map.put("email",c.getString(2));
+            llista.add(map);
+            c.moveToNext();
+        }
+
+        bd.tanca();
+        adapter = new SimpleAdapter(getApplicationContext(), llista, R.layout.activity_item_lista, new String[]{"id","nom","email"},new int[]{R.id.id_item_lista,R.id.nombre_item_lista,R.id.email_item_lista});
+        listView.setAdapter(adapter);
     }
 
 }
